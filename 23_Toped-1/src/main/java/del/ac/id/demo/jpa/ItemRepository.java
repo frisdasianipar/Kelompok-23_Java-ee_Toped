68afcf64fe75152774478ecdedf5a42d46071f69
frisdasianipar <frisdasianipar123@gmail.com>
2020-10-18 21:10:41 +0700

    delete file
    perbaikan file

--- 23_Toped-1/src/main/java/del/ac/id/demo/jpa/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package del.ac.id.demo.jpa;
-
-import org.springframework.data.mongodb.repository.MongoRepository;;
-
-public interface ItemRepository extends MongoRepository<Item, String>{
-	
-}
-
-

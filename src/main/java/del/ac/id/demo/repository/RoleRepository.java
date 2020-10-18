11311cc355ee29efe44141b7a596ec4d8ae42e17
frisdasianipar <frisdasianipar123@gmail.com>
2020-10-18 19:45:42 +0700

    frisda sianipar Update Controller

---
+++ src/main/java/del/ac/id/demo/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package del.ac.id.demo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import del.ac.id.demo.jpa.Role;
+
+public interface RoleRepository extends MongoRepository<Role, String>{
+	
+	Role findByRole(String role);
+}
